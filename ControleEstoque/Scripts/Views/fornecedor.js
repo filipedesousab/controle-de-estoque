f0431fb9258ae62245f76fdb01685b671a9fbb16
Luiz Nunes <luizhgnunes@ymail.com>
2017-10-15 20:10:51 -0300

    Parte da implementação do cadastro do Fornecedor pela interface do usuário.

---
+++ ControleEstoque/Scripts/Views/fornecedor.js
@@ -0,0 +1,21 @@
+﻿var app = angular.module("App", ['ui.bootstrap']);
+
+app.controller('FornecedorController', function ($scope, $http) {
+
+    $scope.adicionarFornecedor = function () {
+        $http({
+            url: '/Pessoa/Adicionar',
+            method: 'POST',
+            data: { firma: $scope.Pessoa }
+        })
+            .success(function (data, status, headers, config) {
+                var msgAlerta = data == -1 ? "Erro no cadastro" : "Cadastrado com sucesso";
+                alert(msgAlerta);
+            })
+            .error(function (data, status, headers, config) {
+                // Lança o erro no console do navegador caso ocorra
+                console.log("Erro: " + status + "\nConfig: " + JSON.stringify(config) + "\nData:\n" + data);
+            });
+    };
+    
+});
