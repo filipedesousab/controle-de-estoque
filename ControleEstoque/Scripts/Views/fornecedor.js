95d140e7efdc4aeb9253ca5d244fca7043ff8b7e
Filipe de Sousa <filipegame07@gmail.com>
2017-10-19 00:55:08 -0200

    Validações

--- ControleEstoque/Scripts/Views/fornecedor.js
+++ ControleEstoque/Scripts/Views/fornecedor.js
@@ -1,38 +1,43 @@
 ﻿var app = angular.module("App", ['ui.bootstrap']);
 
 app.controller('FornecedorController', function ($scope, $http) {
-
+    
     $scope.adicionarFornecedor = function () {
+        var validacao = $.validarCampos(['nome', 'email', 'ddd', 'telefone', 'documento', 'cep', 'endereco', 'bairro', 'cidade', 'uf']);
 
-        $scope.Pessoa.Endereco = document.getElementById('rua').value;
-        $scope.Pessoa.Bairro = document.getElementById('bairro').value;
-        $scope.Pessoa.Municipio = { Id: document.getElementById('ibge').value, Nome: document.getElementById('cidade').value }
-        $scope.Pessoa.TipoDocumento = "CNPJ";
-        $scope.Pessoa.PerfilFornecedor = "S";
-        $scope.Pessoa.PerfilCliente = "N";
-        $scope.Pessoa.PerfilVendedor = "N";
-        $scope.Pessoa.Status = "A";
+        if (validacao) {
+            $scope.Pessoa.Endereco = document.getElementById('rua').value;
+            $scope.Pessoa.Bairro = document.getElementById('bairro').value;
+            $scope.Pessoa.Municipio = { Id: document.getElementById('ibge').value, Nome: document.getElementById('cidade').value }
+            $scope.Pessoa.TipoDocumento = "CNPJ";
+            $scope.Pessoa.PerfilFornecedor = "S";
+            $scope.Pessoa.PerfilCliente = "N";
+            $scope.Pessoa.PerfilVendedor = "N";
+            $scope.Pessoa.Status = "A";
 
-        $http({
-            url: '/Pessoa/Adicionar',
-            method: 'POST',
-            data: { pessoa: $scope.Pessoa }
-        })
-            .success(function (data, status, headers, config) {
-                var msgAlerta = "";
-                if (data == "-1")
-                    msgAlerta = "Erro no cadastro";
-                else if (data == "-2")
-                    msgAlerta = "Já existe uma pessoa cadastrada com esse e-mail.";
-                else
-                    msgAlerta = "Cadastrado com sucesso";
-                alert(msgAlerta);
-                $scope.search();
+            $http({
+                url: '/Pessoa/Adicionar',
+                method: 'POST',
+                data: { pessoa: $scope.Pessoa }
             })
-            .error(function (data, status, headers, config) {
-                // Lança o erro no console do navegador caso ocorra
-                console.log("Erro: " + status + "\nConfig: " + JSON.stringify(config) + "\nData:\n" + data);
-            });
+                .success(function (data, status, headers, config) {
+                    var msgAlerta = "";
+                    if (data == "-1")
+                        msgAlerta = "Erro no cadastro";
+                    else if (data == "-2")
+                        msgAlerta = "Já existe uma pessoa cadastrada com esse e-mail.";
+                    else
+                        msgAlerta = "Cadastrado com sucesso";
+                    alert(msgAlerta);
+                    $scope.search();
+                })
+                .error(function (data, status, headers, config) {
+                    // Lança o erro no console do navegador caso ocorra
+                    console.log("Erro: " + status + "\nConfig: " + JSON.stringify(config) + "\nData:\n" + data);
+                });
+        } else {
+            alert('Preencha os campos corretamente!');
+        }
     };
 
     $scope.search = function(userValue) {
